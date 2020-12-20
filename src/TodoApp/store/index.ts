652eff4287858d0bdca94d369322a949566ea60c
hafeezulkareem <hafeezulkareem20@gmail.com>
2020-12-20 21:16:17 +0530

    Persist todo app data on refresh

--- src/TodoApp/store/index.ts
+++ src/TodoApp/store/index.ts
@@ -2,6 +2,30 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import todosReducer from '../reducers/todosSlice'
 
-export const store = configureStore({ reducer: { todoAppData: todosReducer } })
-
 export type RootState = ReturnType<typeof store.getState>
+
+function saveToLocalStorage(state: RootState) {
+   try {
+      localStorage.setItem('todoAppData', JSON.stringify(state))
+   } catch (e) {
+      console.warn(e)
+   }
+}
+
+function loadFromLocalStorage() {
+   try {
+      const todoAppData = localStorage.getItem('todoAppData')
+      if (todoAppData === null) return undefined
+      return JSON.parse(todoAppData)
+   } catch (e) {
+      console.warn(e)
+      return undefined
+   }
+}
+
+export const store = configureStore({
+   reducer: { todoAppData: todosReducer },
+   preloadedState: loadFromLocalStorage()
+})
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
